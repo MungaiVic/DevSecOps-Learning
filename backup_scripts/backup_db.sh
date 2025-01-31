3306bc18bef7038d07dc85643c51d9c3e8f1da7a
MungaiVic <victornmungai@gmail.com>
2025-01-31 23:34:53 +0300

    Backup Script
    Add environment variables to script

--- backup_scripts/backup_db.sh
+++ backup_scripts/backup_db.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
+source ./.env
 # Variables
-DB_NAME="logistify"
-BACKUP_DIR="/home/monter/dbbackups/"
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 BACKUP_FILE="$BACKUP_DIR/$DB_NAME_$TIMESTAMP.sql.gz"
 
@@ -11,7 +10,7 @@ mkdir -p $BACKUP_DIR
 
 # Dump and compress the database
 echo "Backing up the database $DB_NAME..."
-pg_dump $DB_NAME | gzip > $BACKUP_FILE
+pg_dump -U "$DB_USER" -h "$DB_HOST" $DB_NAME | gzip > "$BACKUP_FILE"
 
 # Check if backup succeeded
 if [ $? -eq 0 ]; then
